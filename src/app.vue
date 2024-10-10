dc3a19b8e2159bcd041283f2d8e33b9e9dee9ec3
sawich <sawich@angeldev.org>
2024-10-10 13:47:22 +0000

    bump deps

--- src/app.vue
+++ src/app.vue
@@ -1,4 +1,6 @@
 <template>
   <RouterView />
+  <!-- https://dev.twitch.tv/docs/chat/irc/ -->
+  <!-- https://dev.to/grim/parsing-ircv3-with-regex-1kcb -->
   <!-- https://youtu.be/CJbEClavywA -->
 </template>
